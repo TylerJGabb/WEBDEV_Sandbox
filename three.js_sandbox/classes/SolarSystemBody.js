db58614948abd5b03c2046629a59552b3f859454
Tyler Gabb <developer@polyprint.com>
2018-02-02 12:20:31 -0700

    mesh standard

--- three.js_sandbox/classes/SolarSystemBody.js
+++ three.js_sandbox/classes/SolarSystemBody.js
@@ -1,11 +1,17 @@
-
-
 var THREE = require('three')
 
 class Ball{
     constructor(radius,color){
         var segments = radius <= 5 ? 15 : 2*radius
         var geom = new THREE.SphereGeometry(radius,segments,segments)
-        var mat = new THREE.MeshLambertMaterial()
+        var mat = new THREE.MeshStandardMaterial({
+            color : color,
+            roughness : 0.4,
+        })
+        this.mesh = new THREE.Mesh(geom,mat)
+    }
+    
+    addToScene(scene){
+        scene.add(this.mesh)
     }
 }
