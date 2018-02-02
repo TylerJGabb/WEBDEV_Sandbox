07bbae001934771e346a1b8c63ddea3f9ca3f45f
Tyler Gabb <developer@polyprint.com>
2018-02-02 13:19:39 -0700

    added sandbox1, sandbox works well now

--- three.js_sandbox/classes/SolarSystemBody.js
+++ three.js_sandbox/classes/SolarSystemBody.js
@@ -1,5 +1,6 @@
-var THREE = require('three')
-
+//var THREE = require('three')
+//https://threejs.org/docs/#api/materials/MeshStandardMaterial
+//Theoretical class for proof of concept
 class Ball{
     constructor(radius,color){
         var segments = radius <= 5 ? 15 : 2*radius
