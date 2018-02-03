d16e2451c59517db59ae9e2633430975f1bb72d6
tylerjgabb <tylerjgabb@gmail.com>
2018-02-03 00:00:02 -0700

    renaming directory

--- three.js_sandbox/classes/SolarSystemBody.js
+++ three.js_sandbox/classes/SolarSystemBody.js
@@ -1,17 +1,53 @@
 //var THREE = require('three')
 //https://threejs.org/docs/#api/materials/MeshStandardMaterial
-//Theoretical class for proof of concept
-class Ball{
-    constructor(radius,color){
+
+/**
+ * Theoretical class for proof of concept.
+ */
+class Sphere{
+    constructor(radius,properties){
         var segments = radius <= 5 ? 15 : 2*radius
         var geom = new THREE.SphereGeometry(radius,segments,segments)
-        var mat = new THREE.MeshStandardMaterial({
-            color : color,
-            roughness : 0.4,
-        })
+        var mat = new THREE.MeshStandardMaterial(properties)
         this.mesh = new THREE.Mesh(geom,mat)
     }
     
+    get color(){
+        return this.mesh.material.color.getHexString()
+    }
+
+    set color(val){
+        this.mesh.material.color.set(val)
+    }
+
+    get emissive(){
+        return this.mesh.material.emissive.getHexString()
+    }
+    
+    set emissive(val){
+        this.mesh.material.emissive.set(val)
+    }
+
+    get position(){
+        return this.mesh.position
+    }
+
+    get metalness(){
+        return this.mesh.material.metalness
+    }
+
+    set metalness(value){
+        this.mesh.material.metalness = value
+    }
+
+    get roughness(){
+        return this.mesh.material.roughness
+    }
+
+    set roughness(value){
+        this.mesh.material.roughness = value
+    }
+
     addToScene(scene){
         scene.add(this.mesh)
     }
